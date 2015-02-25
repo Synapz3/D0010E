994b184168972596976faf0fe6cf21453c69f1fe
Synapz3 <marcus.lund@telia.com>
2015-02-25 15:21:34 +0100

    Release of D0010E
    Not to be copied, just an inspiration of a solution.

--- Labbar/lab5/src/lab5.java
+++ Labbar/lab5/src/lab5.java
@@ -1,22 +1,5 @@
-package tutorial;
-import java.util.Scanner;
-
-public class asdf {
-
+public class Lab5 {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		func(in.nextLine());
-	}
-	
-	public static void func(String a) {
-		for (int i = 0; i < a.length(); i++){
-			int ascii = (int) a.charAt(i);
-			System.out.println("ASCII: "+func2(ascii)+" har nummer: "+ascii);
-		}
-	}
-	
-	public static char func2(int a) {
-		char chars = (char) a;
-		return chars;
+		System.out.println("Template");
 	}
 }
